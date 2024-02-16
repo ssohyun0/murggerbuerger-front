cfb691de411018d3c968212b18a8773801e1fe6b
ssohyun0 <jsh0312033@gmail.com>
2024-02-17 01:50:21 +0900

    css

--- src/comp/game/GamePage.jsx
+++ src/comp/game/GamePage.jsx
@@ -1,30 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import background from '../../assets/img/gamebackground.jpg';
-import IngredientStack from './IngredientStack';
-import OrderStack from './OrderStack';
-
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import background from "../../assets/img/gamebackground.png";
+import IngredientStack from "./IngredientStack";
+import OrderStack from "./OrderStack";
 
 const GamePage = () => {
-    
-    return (
-        <div>
-            <Back>
-                <IngredientStack/>    
-                <OrderStack/>
-            </Back>
-        </div>
-    );
+  return (
+    <Back>
+      <IngredientStack />
+      {/* <OrderStack /> */}
+    </Back>
+  );
 };
 
 export default GamePage;
 
+const div = styled.div``;
+
 const Back = styled.div`
-    display : flex;
-    justify-content: space-between;
-    align-items: center;
-    width : 1280px;
-    height : 720px;
-    background : url(${background});
-    background-size: cover;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #e3dfdf;
+  background-size: cover;
 `;
--- src/comp/game/OrderStack.jsx
+++ src/comp/game/OrderStack.jsx
@@ -1,61 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import IngredientStack from './IngredientStack';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import IngredientStack from "./IngredientStack";
 
 const OrderStack = () => {
-    const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState([]);
 
-    const generateOrders = () => {
-        const newOrders = Array.from({ length: 10 }, (_, index) => {
-            if (index === 0) return 0;
-            if (index === 9) return 7; 
-            return Math.floor(Math.random() * 6) + 1;
-        });
-        setOrders(newOrders);
-    };
-    
-    const mapNumberToIngredient = (number) => {
-        switch(number) {
-            case 0:
-                return '빵';
-            case 1:
-                return '토마토';
-            case 2:
-                return '양상추';
-            case 3:
-                return '양파';
-            case 4:
-                return '베이컨';
-            case 5:
-                return '치즈';
-            case 6:
-                return '패티';
-            case 7:
-                return '빵';
-            default:
-                return '';
-        }
-    };
+  const generateOrders = () => {
+    const newOrders = Array.from({ length: 10 }, (_, index) => {
+      if (index === 0) return 0;
+      if (index === 9) return 7;
+      return Math.floor(Math.random() * 6) + 1;
+    });
+    setOrders(newOrders);
+  };
 
-    useEffect(() => {
-        generateOrders();
-    }, []);
+  const mapNumberToIngredient = (number) => {
+    switch (number) {
+      case 0:
+        return "윗빵";
+      case 1:
+        return "토마토";
+      case 2:
+        return "양상추";
+      case 3:
+        return "양파";
+      case 4:
+        return "베이컨";
+      case 5:
+        return "치즈";
+      case 6:
+        return "패티";
+      case 7:
+        return "아랫빵";
+      default:
+        return "";
+    }
+  };
 
-    return (
-        <div>
-            <h2>주문 내역</h2>
-            <Bill>
-                {orders.map((order, index) => (
-                 <div key={index}>{index + 1}층: {mapNumberToIngredient(order)}</div>
-                ))}
-            </Bill>
-            <IngredientStack orders={orders} />
-        </div>
-    );
+  useEffect(() => {
+    generateOrders();
+  }, []);
+
+  return (
+    <div>
+      <Bill>
+        {orders.map((order, index) => (
+          <div key={index}>
+            {index + 1}층: {mapNumberToIngredient(order)}
+          </div>
+        ))}
+      </Bill>
+      {/* <IngredientStack orders={orders} /> */}
+    </div>
+  );
 };
 
 export default OrderStack;
 
 const Bill = styled.div`
-    
+  font-size: 20px;
+  right: 200px;
+  top: 70px;
+  position: absolute;
+  width: 200px;
+  height: 300px;
+  background-size: cover;
 `;
